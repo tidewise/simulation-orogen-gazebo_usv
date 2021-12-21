9fad70a7112ffe80ee1d5a2a8a1f6cb0abd90e4d
Jhonas <jhonas.moura@tidewise.io>
2021-12-21 09:20:55 -0300

    initial commit

---
+++ gazebo_usvTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef gazebo_usv_TYPES_HPP
+#define gazebo_usv_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace gazebo_usv {
+}
+
+#endif
+
